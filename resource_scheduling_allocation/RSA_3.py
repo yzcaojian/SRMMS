3e8867e46cfdedd9467a30a3ff4707add31fe372
czw <913752926@qq.com>
2021-05-10 19:42:20 +0800

    5月10日摸鱼

--- resource_scheduling_allocation/RSA_3.py
+++ resource_scheduling_allocation/RSA_3.py
@@ -7,21 +7,24 @@ import time
 
 
 # 监控应用需求_服务器失联告警
-def sever_disconnection_warning(ip, io_load_queue, warning_message_queue):
-    # 根据ip选择对应服务器的io负载队列
-    disk_io_data = io_load_queue[ip]
-    # 取出输入队列中最新入队的元素
-    for diskID in disk_io_data:
-        io_data = disk_io_data[diskID][-1]
-        # 获得插入该数据的时间戳  每个时间戳都以自从1970年1月1日午夜（历元）经过了多长时间来表示,单位为秒
-        time_stamp = io_data[-1]
-        now_time = time.time()
-        # 间隔超过十分钟  视作服务器失联
-        if now_time - time_stamp > 600:
-            errorID = 3
-            # 服务器失联异常消息[03, 事件发生时间, 服务器IP, 硬盘标识]
-            warning_message_queue.append([errorID, now_time, ip, diskID])
-        break
+def sever_disconnection_warning(io_load_queue, warning_message_queue):
+    if not io_load_queue:
+        return
+    for ip in io_load_queue:
+        # 根据ip选择对应服务器的io负载队列
+        disk_io_data = io_load_queue[ip]
+        # 取出输入队列中最新入队的元素
+        for diskID in disk_io_data:
+            io_data = disk_io_data[diskID][-1]
+            # 获得插入该数据的时间戳  每个时间戳都以自从1970年1月1日午夜（历元）经过了多长时间来表示,单位为秒
+            time_stamp = io_data[-1]
+            now_time = time.time()
+            # 间隔超过十分钟  视作服务器失联
+            if now_time - time_stamp > 600:
+                errorID = 3
+                # 服务器失联异常消息[03, 事件发生时间, 服务器IP, 硬盘标识]
+                warning_message_queue.append([errorID, now_time, ip, diskID])
+            break
 
 
 # 筛选高IO负载数据
@@ -63,8 +66,8 @@ def hard_disk_high_io_warning(high_io_load_queue, warning_message_queue):
             if now_time - time_stamp > 600:
                 high_io_load_queue[serverIP][diskID] = []
                 continue
-            # 高负载队列数据超过20个，视作该硬盘持续高IO负载
-            if len(high_io_load_queue[serverIP][diskID]) > 20:
+            # 高负载队列数据超过20个,最新插入的数据的时间间隔小于一分钟,视作该硬盘持续高IO负载
+            if len(high_io_load_queue[serverIP][diskID]) > 20 and now_time - time_stamp < 60:
                 sum = 0
                 for item in high_io_load_queue[serverIP][diskID]:
                     sum += item[0]
@@ -72,3 +75,12 @@ def hard_disk_high_io_warning(high_io_load_queue, warning_message_queue):
                 errorID = 4
                 # 硬盘持续高IO异常消息[04, 事件发生时间, 服务器IP, 硬盘标识, 持续期间平均IO负载]
                 warning_message_queue.append([errorID, now_time, serverIP, diskID, average_io])
+
+
+def hard_disk_failutre_warning(hard_disk_failure_prediction_list, warning_message_queue):
+    # 如果列表不为空
+    if hard_disk_failure_prediction_list:
+        ip, disk_id, failure_info = hard_disk_failure_prediction_list
+        health_degree, timestamp = failure_info
+        errorID = 1
+        warning_message_queue.append([errorID, timestamp, ip, disk_id, health_degree])
